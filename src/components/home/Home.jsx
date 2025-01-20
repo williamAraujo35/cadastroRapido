d7401a1bcdf21721ab92fe9a8f44766dea0560a4
william <william.tigoias@gmail.com>
2025-01-20 14:12:28 -0300

    terceira atualização de sistema

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import Main from '../templates/Main';
 
 export default props =>
-    <Main icon='home' title='Início' subtitle='Segundo Projeto utilizando React'>
+    <Main icon='home' title='Início' subtitle='Cadastro'>
             <div className="display-4">Bem Vindo!</div>
             <hr />
-            <p className="mb-0">Sistema para exemplificar a construção de um cadastro desenvolvido em React!</p>
+            <p className="mb-0">Sistema para cadastro rápido de usuário</p>
     </Main>
