2555b11f4c091c0dcccc262eaed43d44f994f9ba
william <william.tigoias@gmail.com>
2025-01-29 21:07:52 -0300

    Updates

--- src/components/home/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Main from '../templates/Main';
-
-export default props =>
-    <Main icon='home' title='Início' subtitle='Cadastro'>
-            <div className="display-4">Bem Vindo!</div>
-            <hr />
-            <p className="mb-0">Sistema para cadastro rápido de usuário</p>
-    </Main>
